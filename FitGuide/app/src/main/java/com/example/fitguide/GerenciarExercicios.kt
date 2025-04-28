e52cdd4169a960001f1802b44b0bf5b405ec7214
bruno-dv2 <brunoge3@edu.unifor.br>
2025-04-28 11:46:27 -0300

    implementa algumas telas do perfil aluno

--- FitGuide/app/src/main/java/com/example/fitguide/GerenciarExercicios.kt
+++ FitGuide/app/src/main/java/com/example/fitguide/GerenciarExercicios.kt
@@ -9,18 +9,26 @@ import com.example.fitguide.databinding.ActivityGerenciarExerciciosBinding
 class GerenciarExercicios : AppCompatActivity() {
 
     private lateinit var binding: ActivityGerenciarExerciciosBinding
+    private var origemAluno = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGerenciarExerciciosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        origemAluno = intent.getBooleanExtra("origemAluno", false)
+
         // Configurar o botão de voltar
         val botaoVoltar: ImageButton = binding.imageButtonBack
         botaoVoltar.setOnClickListener {
-            // Navegar de volta para HomeProfessor
-            val intent = Intent(this, HomeProfessor::class.java)
-            startActivity(intent)
+            // Navegar de volta para HomeProfessor ou HomeAluno
+            if (origemAluno) {
+                val intent = Intent(this, HomeAluno::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, HomeProfessor::class.java)
+                startActivity(intent)
+            }
             finish() // Fechar a atividade atual
         }
 
@@ -53,6 +61,7 @@ class GerenciarExercicios : AppCompatActivity() {
     private fun iniciarCategoria(categoria: String) {
         val intent = Intent(this, Categorias::class.java)
         intent.putExtra("categoria", categoria)
+        intent.putExtra("origemAluno", origemAluno)
         startActivity(intent)
     }
 }
